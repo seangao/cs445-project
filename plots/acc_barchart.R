072ce063a0ff33a0fb7200507a929732567e8403
adrianwlin <linthedrummer@gmail.com>
2016-12-10 16:39:51 -0500

    accuracy barchart

---
+++ plots/acc_barchart.R
@@ -0,0 +1,11 @@
+library(ggplot2)
+acc = c(0.64634146341463417,0.64349593495934965,0.65243902439024393,0.64715447154471539)
+model = c('RF', 'svm','logistic', 'elo')
+acc_df = data.frame(model, acc)
+acc_df$model <- factor(acc_df$model, levels = unique(acc_df$model))
+ggplot(acc_df, aes(x=model, y=acc, fill=model)) +
+  geom_bar(stat="identity") +
+  scale_fill_brewer(palette="Set1") +
+  labs(x = "Model Type", y="Acc", title="Model Accuracy") +
+  coord_cartesian(ylim = c(0.5, 0.7)) 
+ggsave("acc_plot.png")
